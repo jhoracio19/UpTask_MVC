4493c02e441ff2a1252bdf890a366c54b1ac91cc
Jose Horacio <jhoracio19@hotmail.com>
2025-09-22 12:37:19 -0600

    comprobando cuenta del usuario

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -98,10 +98,35 @@ class LoginController {
     }
 
     public static function confirmar(Router $router){
+
+        $token = s($_GET['token']);
+        
+        if(!$token) header('Location: /');
+
+        // Encontrar al usuario con este token
+        $usuario = Usuario::where('token', $token);
+
+        if(empty($usuario)){
+            // No se encontro un usuario con ese token
+            Usuario::setAlerta('error', 'Token No Válido');
+        } else {
+            // Confirmar la cuenta
+            $usuario->confirmado = 1;
+            $usuario->token = '';
+            unset($usuario->password2);
+
+            // Guardar en la base de datos
+            $usuario -> guardar();
+
+            Usuario::setAlerta('exito', 'Cuenta Comprobada Correctamente');
+        }
+
+        $alertas = Usuario::getAlertas();
         
         // Render de la vista
         $router -> render('auth/confirmar', [
-            'titulo' => 'Confirma tu cuenta UpTask'
+            'titulo' => 'Confirma tu cuenta UpTask',
+            'alertas' => $alertas
         ]);
 
     }
